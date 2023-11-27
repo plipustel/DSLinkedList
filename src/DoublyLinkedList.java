ccd89c89c9eaa7d54c43e0c4856f4f646c42b95d
Plipus Telaumbanua <plipus.tel@gmail.com>
2023-11-28 03:09:13 +0700

    DoublyLinkeList is updated

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -2,9 +2,74 @@
  * 
  */
 
-/**
- * 
- */
+class NodePerson {
+	public int id;
+	public String name;
+	public String email;
+
+	NodePerson nextNode;
+	NodePerson prevNode;
+	
+	public NodePerson(int Key, String name, String email) {
+		this.id = Key;
+		this.name = name;
+		this.email = email;
+		this.nextNode = this.prevNode = null;
+	}
+}
+
+
+class DoublyLilink {
+	
+	NodePerson head;
+	NodePerson tail;
+	
+	public DoublyLilink() {
+		this.head = this.tail = null;
+	}
+	
+	public boolean isEmpty() {
+		return (this.head == null);
+	}
+	
+	
+	public void ConstructDoblyLilink(int Key, String name, String email) {
+		
+		NodePerson newNode = new NodePerson(Key, name, email);
+		
+		if(isEmpty()) {
+			this.head = newNode;
+			this.tail = newNode;
+			newNode.nextNode = null;
+			newNode.prevNode = null;
+		} else {
+			newNode.nextNode = head;
+			head.prevNode = newNode;
+			head = newNode;
+		}
+	}
+	
+	public void printDoublyLilink() {
+		NodePerson current = this.head;
+		
+		while(current != null ) {
+			System.out.println(current.id + "\n" + current.name);
+			current = current.nextNode;
+		}
+	}
+	
+	
+	public NodePerson getPrevNode(NodePerson current) {
+	
+		return current.prevNode;
+	}
+	
+	public NodePerson getNextNode(NodePerson current) {
+		
+		return current.nextNode;
+	}
+}
+
 public class DoublyLinkedList {
 
 	/**
@@ -12,7 +77,16 @@ public class DoublyLinkedList {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DoublyLilink HappyFamily = new DoublyLilink();
 
+		HappyFamily.ConstructDoblyLilink(1, "Afrita", "afrita@plipustel.com"); /* tail */
+		HappyFamily.ConstructDoblyLilink(2, "Plipus", "plipus@plipustel.com"); /* node */
+		HappyFamily.ConstructDoblyLilink(3, "Adam", "adam@plipustel.com"); /* head */
+		
+		HappyFamily.printDoublyLilink();
+		
+	//	System.out.println(HappyFamily.getPrevNode(1));
+		
 	}
 
 }
